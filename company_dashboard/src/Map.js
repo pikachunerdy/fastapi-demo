4ad5b48f5da6485bdc808de5f696d115e4063c21
pikachunerdy <fullbravery@gmail.com>
2022-09-28 20:42:10 +0000

    map

--- company_dashboard/src/Map.js
+++ company_dashboard/src/Map.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+// import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 import { getRecoil } from 'recoil-nexus';
 import { deviceListState, panelSizes } from './atoms';
 import { auth_manager, device_list_manager } from './managers';
-
+//     "google-maps-react": "*",
 var mapStyles = {
   width: '0px',
   height: '0px',
   zIndex: '-10'
 };
 
-export class MapContainer extends Component {
+export default class MapContainer extends Component {
   componentDidMount() {
     document.getElementById("mapID").firstChild.firstChild.firstChild.style.width = "0px";
     document.getElementById("mapID").firstChild.firstChild.firstChild.style.height = "0px";    // set el height and width etc.
@@ -22,26 +22,27 @@ export class MapContainer extends Component {
     };
     var devices = getRecoil(deviceListState);
     return (
-      <Map
-        resetBoundsOnResize={true}
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={
-          {
-            lat: 51.49,
-            lng: -0.183
-          }
-        }
-      >
-        {devices.devices.map((device) => {
-          return (<Marker position={{lat : device.latitude, lng : device.longitude}} onClick={() => device_list_manager.select_device(device.device_id)}/>)
-        })}
-      </Map>
+      // <Map
+      //   resetBoundsOnResize={true}
+      //   google={this.props.google}
+      //   zoom={14}
+      //   style={mapStyles}
+      //   initialCenter={
+      //     {
+      //       lat: 51.49,
+      //       lng: -0.183
+      //     }
+      //   }
+      // >
+      //   {devices.devices.map((device) => {
+      //     return (<Marker position={{lat : device.latitude, lng : device.longitude}} onClick={() => device_list_manager.select_device(device.device_id)}/>)
+      //   })}
+      // </Map>
+      <div>Map</div>
     );
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyAXrhOvVPfXbX4svEMfTXnxIXNFkOyl474'
-})(MapContainer);
+// export default GoogleApiWrapper({
+//   apiKey: 'AIzaSyAXrhOvVPfXbX4svEMfTXnxIXNFkOyl474'
+// })(MapContainer);
